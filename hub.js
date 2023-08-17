1d38640b7a85a1309812b1cfbcae9872609b5845
EMILY GREASON <GREASONEMILY@GMAIL.COM>
2023-08-16 18:44:50 -0700

    added payload.timestamp to console logs

--- hub.js
+++ hub.js
@@ -9,12 +9,12 @@ io.listen(3000);
 const reach = io.of('/reach');
 
 function handleSendLocation(payload) {
-  console.log(`My current location is lat:${payload.lat} lon:${payload.lon}`);
+  console.log(`My current location is lat:${payload.lat} lon:${payload.lon}`, payload.timestamp);
   reach.emit(events.sendLocation);
 }
 
 function handleReceiveLocation(payload) {
-  console.log(`Other users can now see the location for ${payload.username}`);
+  console.log(`Other users can now see the location for ${payload.username}`, payload.timestamp);
   reach.emit(events.receiveLocation, {
     username: payload.username,
     lat: payload.lat,
