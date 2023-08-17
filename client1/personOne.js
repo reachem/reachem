73f7c6548c59a3641ad0bf67e9bba132dc994a89
Michael Pace <michael.g.pace@gmail.com>
2023-08-16 19:44:10 -0500

    basic files built for clients

--- client1/personOne.js
+++ client1/personOne.js
@@ -1,18 +1,49 @@
 'use strict';
 
-const { io } = require('socket.io-client');
+const { chance, EVENT_NAMES } = require('../util');
 
-const client = io('ws://localhost:3000/caps');
-client.on(events.announcement, (payload) => console.log(payload.message));
-client.on(events.ready, pickup);
+function sendPickup(events) {
+  const event = {
+    // store: chance.city(),
+    // orderId: chance.guid(),
+    // customer: chance.name(),
+    // address: chance.address(),
+    // company: 'Flowers',
+  };
 
-// pickup(events);
+  const payload = {
+    event: 'pickup', // either pickup or delivered
+    messageId: event.orderId, // unique id from the original payload
+    clientId: `1-800-flowers`, // either acme-widgets or 1-800-flowers
+    order: event,
+  };
 
-client.on(pickup, (payload) => {
-  pickupConfirmation(payload, client)
-  setTimeout(() => {
-    deliveredConfirmation(payload, client)}
-    , 5000);
-});
+  console.log('Vendor asking for pickup!', event);
+  events.emit(EVENT_NAMES.pickup, payload);
+}
+function acknowledgePost(payload, client) {
+  console.log('Thank you for posting!', payload.messageId);
+  client.emit('received', payload);
+}
 
-module.exports = { client };
+
+
+function startClient(client) {
+  console.log('Vendor is started');
+  client.emit('getAll', 'acme-widgets');
+  client.on(EVENT_NAMES.delivered, (payload) =>
+    acknowledgePost(payload, client)
+  );
+  
+  function ready() {
+    // sends an initial pickup event, then sets a timer for 4-5 seconds and repeats
+    sendPickup(client);
+    setTimeout(ready, chance.integer({ min: 5000, max: 10000 }));
+  }
+  // calls itself every 4 - 5 seconds
+  ready();
+}
+
+module.exports = {startClient,
+  toTest: {sendPickup, acknowledgePost}
+};
--- clients/personThree.js
+++ clients/personThree.js
@@ -1,10 +1,49 @@
 'use strict';
 
-const { chance, EVENT_NAMES } = require('..')
+const { chance, EVENT_NAMES } = require('../util');
 
-function start(client) {
-  console.log("Vendor is started");
-  client.emit("getAll", "acme-widgets");
+function sendPickup(events) {
+  const event = {
+    // store: chance.city(),
+    // orderId: chance.guid(),
+    // customer: chance.name(),
+    // address: chance.address(),
+    // company: 'Flowers',
+  };
+
+  const payload = {
+    event: 'pickup', // either pickup or delivered
+    messageId: event.orderId, // unique id from the original payload
+    clientId: `1-800-flowers`, // either acme-widgets or 1-800-flowers
+    order: event,
+  };
+
+  console.log('Vendor asking for pickup!', event);
+  events.emit(EVENT_NAMES.pickup, payload);
+}
+function acknowledgePost(payload, client) {
+  console.log('Thank you for posting!', payload.messageId);
+  client.emit('received', payload);
+}
+
+
+
+function startClient(client) {
+  console.log('Vendor is started');
+  client.emit('getAll', 'acme-widgets');
   client.on(EVENT_NAMES.delivered, (payload) =>
-    acknowledgeDelivery(payload, client)
+    acknowledgePost(payload, client)
   );
+  
+  function ready() {
+    // sends an initial pickup event, then sets a timer for 4-5 seconds and repeats
+    sendPickup(client);
+    setTimeout(ready, chance.integer({ min: 5000, max: 10000 }));
+  }
+  // calls itself every 4 - 5 seconds
+  ready();
+}
+
+module.exports = {startClient,
+  toTest: {sendPickup, acknowledgePost}
+};
