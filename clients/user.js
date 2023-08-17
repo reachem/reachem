704bcfc9f652df540827cc5ce3cda213159baab4
EMILY GREASON <GREASONEMILY@GMAIL.COM>
2023-08-16 19:56:17 -0700

    adjusted client file

--- clients/user.js
+++ clients/user.js
@@ -1,35 +1,37 @@
 'use strict';
 
 const { io } = require('socket.io-client');
-const { example, Example } = require('../clients/example');
+const events = require('../eventPool');
+const { example } = require('../clients/example');
 const client = io('ws://localhost:3000/reach');
-const { sendLocation, receiveLocation, confirmLocation } = require('../eventPool');
-const { handleSendLocation, handleReceiveLocation, handleConfirmLocation } = require('../hub');
-
-const person = new Example();
 
 
+reach.emit(events.sendLocation, payload);
 
-if (handleSendLocation()) {
-  console.log('Post!');
-  client.emit(sendLocation, example);
-}
+reach.on(events.receiveLocation, (example) => console.log(`Other users can now see the location for ${example.username}`, payload.timestamp));
+reach.on(events.confirmLocation, (example) => console.log('Location confirmed! I see you!', example.username));
+// const person = new Example();
 
-if (handleReceiveLocation(example, receiveLocation)) {
-  console.log('Location received!', example);
-  client.on('received', example);
-}
+// if (handleSendLocation()) {
+//   console.log('Post!');
+//   client.emit(sendLocation, example);
+// }
 
-if (handleConfirmLocation(confirmLocation)) {
-  console.log('Location is confirmed');
-  client.on(confirmLocation, (example) =>
-    handleSendLocation(example, confirmLocation)
-  );
-  client.on('request', () => {
-    console.log('sending info', person);
-    client.emit(person);
-  });
+// if (handleReceiveLocation(example, receiveLocation)) {
+//   console.log('Location received!', example);
+//   client.on('received', example);
+// }
 
-}
+// if (handleConfirmLocation(confirmLocation)) {
+//   console.log('Location is confirmed');
+//   client.on(confirmLocation, (example) =>
+//     handleSendLocation(example, confirmLocation)
+//   );
+//   client.on('request', () => {
+//     console.log('sending info', person);
+//     client.emit(person);
+//   });
 
-module.exports = { };
+// }
+
+module.exports = { client };
