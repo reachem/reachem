0c6ae03512f165513f63f5c7df28df222e98b556
rpruazol <rpruazol@gmail.com>
2023-08-16 21:52:28 -0400

    example payload class

--- clients/example.js
+++ clients/example.js
@@ -8,7 +8,7 @@ const chance = new Chance();
 
 class Example {
   constructor(){
-    this.username = chance.string({length: 8})
+    this.username = chance.word({length: 8})
     this.email = chance.email();
     this.lat= chance.latitude();
     this.long= chance.longitude();
